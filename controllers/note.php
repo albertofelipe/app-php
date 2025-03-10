91a556452a4ea5de8a7e64cece87aba0219c7212
Alberto Felipe <felipe0032sena@gmail.com>
2025-03-10 13:59:32 -0300

    3 - authorization intro and rewriting

--- controllers/note.php
+++ controllers/note.php
@@ -5,6 +5,10 @@ $db = new Database($config['database']);
 
 $heading = 'Note';
 
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where id = :id', [
+   'id' => $_GET['id']
+])->findOrfail();
+
+authorize($note['user_id'] !== 5);
 
 require "views/note.view.php";
